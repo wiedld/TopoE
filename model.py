442b803651d70898989c7fc61f3df02b8a7f942d
Hackbright Student <info@hackbrightacademy.com>
2015-02-17 16:02:13 -0800

    database seeding is working for StatsGen.

--- model.py
+++ model.py
@@ -1,69 +1,40 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
-from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
+# from sqlalchemy.orm import relationship, backref, scoped_session
 
 
 Base = declarative_base()
+
+#######################################################################
 
 ##  STEP1 = make the db file and metadata.  In python shell:
 # -i model.py  >  create_engine  >  Base.metadata.create
 
 ##  STEP 2 = Perform seeding.
-# ENGINE = None 
-# Session = None
-# def connect():
-# 	global ENGINE
-# 	global Session
-# 	ENGINE = create_engine("sqlite:///database.db", echo=True)
-# 	Session = sessionmaker(bind=ENGINE)
-# 	return Session
+ENGINE = None 
+Session = None
+# function connect() at bottom...before main.
 ## in python shell:  -i model.py > s=connect() >  
 
 ##  STEP 3 = setup threading.
-##  remove step2 code.   Instead, do below.
-# ENGINE = create_engine("sqlite:///database.db", echo=True)
+##  remove step2 code.   import on line 5.  Insert below.
+# ENGINE = create_engine("sqlite:///generators.db", echo=True)
 # session = scoped_session(sessionmaker(bind=ENGINE, autocommit=False, autoflush=False))
 # Base.query = session.query_property()
 
+######################################################################
 
 
-
-# Static data from the EIA860 csv file. Generator location.
-# TODO: FIND LOCATION FOR 10 GENERATORS IN 923, MISSING FROM 860.
-class StatsGen(Base):
-	__tablename__ = "StatsGens"
-	id = Column(Integer, primary_key=True)
-	utility_id = Column(String(15))
-	utility_name = Column(String(50))
-	plant_name = Column(String(100), ForeignKey("ProdGens.plant_name"))
-	state = Column(String(15))
-	county = Column(String(30))
-	status = Column(String(15))
-	nameplate_MW = Column(Float)
-	summer_MW = Column(Float)
-	winter_MW = Column(Float)
-	start_mo = Column(String(2))
-	start_yr = Column(String(4))
-	fuel_1 = Column(String(10))
-	fuel_2 = Column(String(10))
-	fuel_3 = Column(String(10))
-	fuel_4 = Column(String(10))
-	fuel_5 = Column(String(10))
-	fuel_6 = Column(String(10))
-	multi_fuel = Column(String(1))
-	interconnected = Column(String(1))
-	synchronized = Column(String(1))
-
-
-
+##  This table is based on the most recent generator data (Nov 2014), and therefore has the most complete dataset.  Have this be the central database, because it lists ALL GENERATORS.
 # Static data from the EIA923 csv files. Generator production.
 class ProdGen(Base):
 	__tablename__ = "ProdGens"
 	id = Column(Integer, primary_key=True)
 	plant_id = Column(String(15))
 	chp = Column(String(5))
-	plant_name = Column(String(100), ForeignKey("StatsGens.plant_name"))
+	plant_name = Column(String(100))	# plant names are not novel.  cannot be key.
 	operator_name = Column(String(100))
 	operator_id = Column(String(15))
 	state = Column(String(2))
@@ -103,6 +74,42 @@ class ProdGen(Base):
 	dec_mwh_gen = Column(Integer)
 
 
+
+
+# Static data from the EIA860 csv file. Generator location.
+# TODO: FIND LOCATION FOR 10 GENERATORS IN 923, MISSING FROM 860.
+class StatsGen(Base):
+	__tablename__ = "StatsGens"
+	id = Column(Integer, primary_key=True)
+	utility_id = Column(String(15))
+	utility_name = Column(String(50))
+	plant_name = Column(String(100))	# plant names are not novel.  cannot be key.
+	state = Column(String(15))
+	county = Column(String(30))
+	status = Column(String(15))
+	nameplate_MW = Column(Float)
+	summer_MW = Column(Float)
+	winter_MW = Column(Float)
+	start_mo = Column(DateTime)
+	fuel_1 = Column(String(10))
+	fuel_2 = Column(String(10))
+	fuel_3 = Column(String(10))
+	fuel_4 = Column(String(10))
+	fuel_5 = Column(String(10))
+	fuel_6 = Column(String(10))
+	multi_fuel = Column(String(1))
+	interconnected = Column(String(1))
+	synchronized = Column(String(1))
+
+
+######################################################################
+
+def connect():
+	global ENGINE
+	global Session
+	ENGINE = create_engine("sqlite:///generators.db", echo=True)
+	Session = sessionmaker(bind=ENGINE)
+	return Session()
 
 def main():
 	"""For future use."""
--- seeding.py
+++ seeding.py
@@ -1,23 +1,45 @@
 import model
 import csv
+from datetime import datetime
+
+
+
+def load_gen_prod(session):
+	pass
+
+
 
 
 def load_gen_stats(session):
-	with open('seed_data/test.csv', 'rb') as csvfile:
+	# with open('seed_data/test.csv', 'rb') as csvfile:
+	with open('seed_data/gen_stats.csv', 'rb') as csvfile:
 		reader = csv.reader(csvfile, dialect='excel')
 		for row in reader:
+			print ("row:", row)		# troubleshooting.  If issue, see where.
 			gen_obj = model.StatsGen()
+
 			gen_obj.utility_id = row[0]
 			gen_obj.utility_name = row[1]
 			gen_obj.plant_name = row[3]
 			gen_obj.state = row[4]
 			gen_obj.county = row[5]
 			gen_obj.status = row[8]
-			gen_obj.nameplate_MW = float(row[9])
-			gen_obj.summer_MW = float(row[10])
-			gen_obj.winter_MW = float(row[11])
-			gen_obj.start_mo = row[13]
-			gen_obj.start_yr = row[14]
+
+			#  some of the data is written as x,xxx.   Or is blank.
+			if (row[9] is not None) and (row[9] is not ""):
+				gen_obj.nameplate_MW = float(  (row[9]).replace(",", "") )
+			if (row[10] is not None) and (row[10] is not ""):
+				gen_obj.summer_MW = float( (row[10]).replace(",","") )
+			if (row[11] is not None) and (row[11] is not ""):
+				gen_obj.winter_MW = float( (row[11]).replace(",","") )
+
+			#  a few of the months are incorrect.  E.g. "88" as a month.
+			if int(row[13])>0 and int(row[13])<13:
+				starting_mo_yr = str(row[13]) + "-" + str(row[14])
+				gen_obj.start_mo = datetime.strptime(starting_mo_yr, "%m-%Y")
+			else:
+				gen_obj.start_mo = datetime.strptime(row[14], "%Y")
+
 			gen_obj.fuel_1 = row[15]
 			gen_obj.fuel_2 = row[16]
 			gen_obj.fuel_3 = row[17]
@@ -29,11 +51,9 @@ def load_gen_stats(session):
 			gen_obj.synchronized = row[23]
 
 			session.add(gen_obj)
-	session.commit()
 
+		session.commit()
 
-def load_gen_prod(session):
-	pass
 
 
 
